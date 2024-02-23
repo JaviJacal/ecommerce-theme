7b8233c0087ff501eaf24d3f7766036046b20d88
Ramirez, Radames <zerimar.nada@gmail.com>
2024-02-23 09:49:59 -0600

    3 testimonios agregados

--- src/pages/testimonios.js
+++ src/pages/testimonios.js
@@ -2,12 +2,10 @@ import React, { useRef } from 'react';
 
 import Container from '../components/Container';
 import Hero from '../components/Hero';
-import ThemeLink from '../components/ThemeLink';
 import Layout from '../components/Layout/Layout';
 
 import * as styles from './testimonios.module.css';
 const TestimoniosPage = (props) => {
-  let historyRef = useRef();
   let valuesRef = useRef();
   let sustainabilityRef = useRef();
 
@@ -25,100 +23,54 @@ const TestimoniosPage = (props) => {
       <div className={styles.root}>
         {/* Hero Container */}
         <Hero
-          maxWidth={'900px'}
-          image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          maxWidth={'500px'}
+          image={'/BannerTestimonios.jpg'}
+          title={`Testimonios`}
         />
 
-        <div className={styles.navContainer}>
-          <ThemeLink onClick={() => handleScroll(historyRef)} to={'#history'}>
-            History
-          </ThemeLink>
-          <ThemeLink onClick={() => handleScroll(valuesRef)} to={'#values'}>
-            Values
-          </ThemeLink>
-          <ThemeLink
-            onClick={() => handleScroll(sustainabilityRef)}
-            to={'#sustainability'}
-          >
-            Sustainability
-          </ThemeLink>
+        <div className={styles.imageContainer} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px'}}>
+          <img 
+            alt={'La oportunidad laboral'}
+            src={'/Testimonio1Empresario.png'} 
+            style={{ maxWidth: '800px', height: 'auto' }}        
+          />
         </div>
-
-        <Container size={'large'} spacing={'min'}>
-          <div className={styles.detailContainer} ref={historyRef}>
-            <p>
-              Founded in 1860, Sydney is an innovative British brand with a
-              contemporary edge. We make timeless everyday luxury clothing.
-            </p>
-            <br />
-            <br />
-            <p>
-              We created some of the world's first T-shirts and spent decades
-              perfecting the feel of the cotton. Today we are the only brand
-              that makes T-shirts in its own factory in the UK. And we do this
-              in the same factory we have occupied since 1937.
-            </p>
-          </div>
-        </Container>
-
-        <div className={styles.imageContainer}>
-          <img alt={'shirt brand'} src={'/about1.png'}></img>
+        
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'justify', marginTop: '10px' }}>
+          <p>"Siempre fui escéptico respecto a las lecturas del tarot y la clarividencia, hasta que un día, llevado por la curiosidad y un poco de desesperación, decidí visitar a Ezequiel. Estaba atravesando un momento complicado en mi carrera; sentía que había alcanzado un techo y no veía salida. Ezequiel, con solo echar un vistazo a sus cartas, predijo que estaba a punto de presentarse una oportunidad laboral inesperada, pero que debía ser valiente para tomarla, ya que implicaría un gran cambio. No pasaron ni dos semanas cuando me llegó una oferta de trabajo de otro país, algo que jamás había considerado. Recordando sus palabras, acepté. Hoy, estoy viviendo la mejor etapa de mi vida profesional, en un lugar que nunca imaginé. La precisión de Ezequiel fue asombrosa, y su consejo, invaluable."</p>
         </div>
-
-        <Container size={'large'} spacing={'min'}>
-          <div className={styles.content}>
-            <h3>Our Values</h3>
-            <div ref={valuesRef}>
-              <p>
-                Sunspel produced some of the world's earliest T-shirts. In the
-                late 1800s the business made luxury tunics and undershirts from
-                lightweight Sea Island cotton for export to the Far East and
-                other warm climates. While these garments initially had silk
-                buttoned plackets, these were removed in the early 1900s and
-                replaced with simple bound necks to reduce manufacturing costs -
-                creating the T-shirt. We've supplied the world as the T-shirt
-                has evolved from underwear to outerwear, from symbol of youthful
-                rebellion to everyday wardrobe staple, and we've spent decades
-                refining its every last aspect.
-              </p>
-              <ol>
-                <li>Be an ecowear</li>
-                <li>Sophisticated and not mass-produced</li>
-                <li>Only natural materials</li>
-              </ol>
-              <img alt={'founder'} src={'/about2.png'}></img>
-            </div>
-            <h3>Sustainability</h3>
-            <div id={'#sustainability'} ref={sustainabilityRef}>
-              <p>
-                Our founder, Thomas Hill, had both an eye for quality and a
-                desire to innovate. As well as using the finest fibres such as
-                Sea Island cotton, cashmere and silk, he invented his own
-                fabrics. Sunspel continues this commitment to innovation today
-                and our unique fabrics include: Q100 Sea Island cotton, Q82
-                Supima cotton, Q75 warp knit mesh cotton and Q14 warp knit
-                cellular cotton. The technology behind these fabrics remains
-                unchanged today and all Sunspel products use the finest cottons,
-                wools and fibres.
-              </p>
-              <p>
-                Made in Long Eaton, England and crafted from our luxurious long
-                staple Supima cotton for unparalleled softness, comfort and
-                durability, the Sunspel T-shirt has a classic fit and only the
-                most essential details.{' '}
-              </p>
-              <p>
-                With over 100 years spent perfecting fabric, fit and style, the
-                Sunspel Classic T-shirt is recognised as the finest in the
-                world.
-              </p>
-            </div>
-          </div>
-        </Container>
-
-        <div className={styles.imageContainer}>
-          <img alt={'shirt backwards'} src={'/about3.png'}></img>
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'right', marginTop: '00px' }}>
+          <p>David N.</p>
+        </div>
+        
+        <div className={styles.imageContainer} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px'}}>
+          <img 
+            alt={'El Amor Verdadero'}
+            src={'/MujerLatina.png'} 
+            style={{ maxWidth: '800px', height: 'auto' }}        
+          />
+        </div>
+        
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'justify', marginTop: '10px' }}>
+          <p>"Nunca olvidaré el día que Ezequiel cambió mi vida amorosa para siempre. Después de varios fracasos sentimentales, había perdido la fe en el amor. Decidí buscar la guía de Ezequiel como último recurso, sin esperar mucho. En su lectura, mencionó que el amor estaba más cerca de lo que pensaba, pero que debía abrir mi corazón y dejar ir el pasado para encontrarlo. Además, especificó que encontraría a esta persona en un lugar relacionado con el arte. Aunque me pareció extraño, decidí asistir a más eventos culturales. Un mes después, en una exposición de fotografía, conocí a Alex. Resultó ser todo lo que Ezequiel había descrito y más. Estamos juntos desde entonces, y nunca he sido más feliz. La precisión de Ezequiel me llevó directo a mi alma gemela."</p>
+        </div>
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'right', marginTop: '00px' }}>
+          <p>Silvia G.</p>
+        </div>
+        
+        <div className={styles.imageContainer} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px'}}>
+          <img 
+            alt={'El Accidente Evitado'}
+            src={'/HombreMaduro.png'} 
+            style={{ maxWidth: '500px', height: 'auto' }}  
+          />
+        </div>
+        
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'justify', marginTop: '10px' }}>
+          <p>"La lectura de Ezequiel me salvó la vida, literalmente. Durante una consulta casual, una de sus predicciones me dejó helado. Me advirtió sobre un grave peligro en un viaje próximo y me aconsejó posponerlo si era posible. Aunque tenía todo planeado y me costaba creer en su advertencia, algo en su tono me hizo escuchar. Decidí retrasar el viaje. El día que originalmente habría estado volando, el vuelo en el que habría estado se vio involucrado en un accidente. Aunque afortunadamente no hubo víctimas fatales, el susto y la posibilidad de lo que podría haber pasado me persiguen hasta hoy. Estoy eternamente agradecido por la intervención de Ezequiel; su visión me salvó de un destino incierto."</p>
+        </div>
+        <div style={{ maxWidth: '900px', margin: '0 auto', textAlign: 'right', marginTop: '00px' }}>
+          <p>Jorge R.</p>
         </div>
       </div>
     </Layout>
